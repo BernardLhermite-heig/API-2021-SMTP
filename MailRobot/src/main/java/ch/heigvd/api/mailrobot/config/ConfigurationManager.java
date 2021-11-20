516110a7e726a17dd06a8a372393e964cb11a12e
BernardLhermite <stephane.marengo@hotmail.com>
2021-11-20 16:35:58 +0100

    Implémentation du ConfigurationManager Ajouts de tests

--- MailRobot/src/main/java/ch/heigvd/api/mailrobot/config/ConfigurationManager.java
+++ MailRobot/src/main/java/ch/heigvd/api/mailrobot/config/ConfigurationManager.java
@@ -1,17 +1,22 @@
 package ch.heigvd.api.mailrobot.config;
 
+import ch.heigvd.api.mailrobot.model.mail.Person;
 import lombok.Getter;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Properties;
-import java.util.Scanner;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ConfigurationManager {
     private final static Logger LOG = Logger.getLogger(ConfigurationManager.class.getName());
+    private static final String DEFAULT_MESSAGE_SEPARATOR = "---";
+    private static final String DEFAULT_PERSON_SEPARATOR = ":";
 
     @Getter
     private String serverAddress;
@@ -20,50 +25,137 @@ public class ConfigurationManager {
     @Getter
     private int numberOfGroups;
     @Getter
-    private String witnessAddress;
+    private List<String> witnessesAddresses;
     private String messageSeparator;
+    private String personSeparator;
 
-//    private final List<Person> persons;
-//    private final List<String> messages;
+    private final List<Person> persons;
+    private final List<String> messages;
 
 
     public ConfigurationManager(String configFile, String messagesFile, String personFile) throws IOException {
+        loadProperties(configFile);
 
-        loadProperties(parseFileName(configFile));
-        loadMessages(parseFileName(messagesFile));
+        messages = loadMessages(messagesFile);
+        persons = loadPersons(personFile);
     }
 
-    private InputStream getResourceAsStream(String fileName) {
-        return ConfigurationManager.class.getResourceAsStream(fileName);
+    /**
+     * Retourne un InputStream sur le fichier de resource passé en paramètre.
+     *
+     * @param fileName le nom du fichier de resource
+     * @return un InputStream sur le fichier
+     * @throws IOException si le fichier ne peut pas être ouvert
+     */
+    private InputStream getResourceAsStream(String fileName) throws IOException {
+        InputStream in = ConfigurationManager.class.getResourceAsStream(parseFileName(fileName));
+        if (in == null)
+            throw new IOException("File " + fileName + " not found");
+
+        return in;
     }
 
+    /**
+     * Retourne le chemin complet du fichier de resource passé en paramètre.
+     *
+     * @param fileName le nom du fichier de resource
+     * @return le chemin complet du fichier
+     * @throws IOException si le fichier ne peut pas être ouvert
+     */
+    private String getResource(String fileName) throws IOException {
+        URL path = ConfigurationManager.class.getResource(parseFileName(fileName));
+        if (path == null)
+            throw new IOException("File " + fileName + " not found");
+
+        return path.getPath();
+    }
+
+    /**
+     * Ajoute un '/' au nom de fichier passé en paramètre si ce dernier est manquant.
+     *
+     * @param fileName le nom du fichier à traiter
+     * @return le nom du fichier sous la forme /nomFichier
+     */
     private String parseFileName(String fileName) {
         return fileName.startsWith("/") ? fileName : "/" + fileName;
     }
 
+    /**
+     * Charge les données du fichier de propriétés passé en paramètre.
+     * // TODO throw si donnée manquante?
+     *
+     * @param fileName le fichier à lire
+     * @throws IOException si le fichier ne peut pas être lu
+     */
     private void loadProperties(String fileName) throws IOException {
         try (InputStream in = getResourceAsStream(fileName)) {
             Properties prop = new Properties();
             prop.load(in);
 
+            //todo check donnée
             serverAddress = prop.getProperty("serverAddress");
             serverPort = Integer.parseInt(prop.getProperty("serverPort"));
             numberOfGroups = Integer.parseInt(prop.getProperty("numberOfGroups"));
-            witnessAddress = prop.getProperty("witnessAddress");
-            messageSeparator = prop.getProperty("messageSeparator");
+            messageSeparator = prop.getProperty("messageSeparator", DEFAULT_MESSAGE_SEPARATOR);
+            personSeparator = prop.getProperty("personSeparator", DEFAULT_PERSON_SEPARATOR);
+
+            String witnesses = prop.getProperty("witnessAddress");
+            witnessesAddresses = new ArrayList<>(Arrays.asList(witnesses.split(":")));
         } catch (IOException e) {
             LOG.log(Level.SEVERE, "Error while parsing configuration file. ", e);
             throw e;
         }
     }
 
-    private void loadMessages(String fileName) {
-        Scanner scan = new Scanner(getResourceAsStream(fileName), StandardCharsets.UTF_8);
-        scan.useDelimiter(messageSeparator);
+    /**
+     * Retourne une liste des messages situés dans le fichier passé en paramètre.
+     *
+     * @param fileName le fichier à lire
+     * @throws IOException si le fichier ne peut pas être lu
+     */
+    private List<String> loadMessages(String fileName) throws IOException {
+        List<String> messages = new LinkedList<>();
 
-        while (scan.hasNext()) {
-            System.out.println(scan.next());
+        try (Scanner scan = new Scanner(getResourceAsStream(fileName), StandardCharsets.UTF_8)) {
+            scan.useDelimiter(messageSeparator);
+
+            while (scan.hasNext()) {
+                messages.add(scan.next());
+            }
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, "Error while parsing messages file. ", e);
+            throw e;
         }
-        scan.close();
+        return messages;
+    }
+
+    /**
+     * Retourne une liste des adresses situées dans le fichier passé en paramètre.
+     *
+     * @param fileName le fichier à lire
+     * @throws IOException si le fichier ne peut pas être lu
+     */
+    private List<Person> loadPersons(String fileName) throws IOException {
+        List<Person> persons = new LinkedList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(getResource(fileName), StandardCharsets.UTF_8))) {
+            for (String line; br.ready() && (line = br.readLine()) != null; ) {
+                String[] person = line.split(personSeparator);
+                //todo
+                //persons.add(new Person());
+            }
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, "Error while parsing persons file. ", e);
+            throw e;
+        }
+        return persons;
+    }
+
+    public List<Person> getPersons() {
+        return Collections.unmodifiableList(persons);
+    }
+
+    public List<String> getMessages() {
+        return Collections.unmodifiableList(messages);
     }
 }
--- MailRobot/src/test/java/ch/heigvd/api/mailrobot/config/ConfigurationManagerTest.java
+++ MailRobot/src/test/java/ch/heigvd/api/mailrobot/config/ConfigurationManagerTest.java
@@ -3,37 +3,49 @@ package ch.heigvd.api.mailrobot.config;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ConfigurationManagerTest {
-    private static final Logger LOG = Logger.getLogger(ConfigurationManagerTest.class.getName());
     private static final String CORRECT_CONFIG = "config.properties";
     private static final String EMPTY_FILE = "empty.txt";
     private static final String INVALID_FILE = "unknown.txt";
+    private static final String MESSAGE_FILE = "messages.txt";
+    private static final String TARGET_FILE = "targets.txt";
     private static final String ADDRESS = "localhost";
 
     private static final int PORT = 25;
     private static final int NB_GROUPS = 10;
-    private static final String WITNESS_ADDRESS = "witness.address@heig-vd.ch";
-    private static final String MESSAGE_SEPARATOR = "---";
+    private static final String[] WITNESS_ADDRESS = {"witness.address@heig-vd.ch"};
+
+    private static final int MESSAGE_COUNT = 4;
+    private static final int TARGET_COUNT = 5;
 
     @Test
     void itShouldCorrectlyReadTheConfigFile() throws IOException {
         ConfigurationManager manager = new ConfigurationManager(CORRECT_CONFIG, EMPTY_FILE,
                 EMPTY_FILE);
 
-        assertEquals(manager.getServerAddress(), ADDRESS);
-        assertEquals(manager.getServerPort(), PORT);
-        assertEquals(manager.getNumberOfGroups(), NB_GROUPS);
-        assertEquals(manager.getWitnessAddress(), WITNESS_ADDRESS);
+        assertEquals(ADDRESS, manager.getServerAddress());
+        assertEquals(PORT, manager.getServerPort());
+        assertEquals(NB_GROUPS, manager.getNumberOfGroups());
+        assertArrayEquals(WITNESS_ADDRESS, manager.getWitnessesAddresses().toArray());
     }
+
     @Test
     void itShouldThrowOnInvalidFile() {
-        Exception exception = assertThrows(IOException.class, () -> {
-            new ConfigurationManager(INVALID_FILE, EMPTY_FILE, EMPTY_FILE);
-        });
+        assertThrows(IOException.class, () -> new ConfigurationManager(INVALID_FILE, EMPTY_FILE, EMPTY_FILE));
+        assertThrows(IOException.class, () -> new ConfigurationManager(CORRECT_CONFIG, INVALID_FILE, EMPTY_FILE));
+        assertThrows(IOException.class, () -> new ConfigurationManager(CORRECT_CONFIG, EMPTY_FILE, INVALID_FILE));
     }
+
+    @Test
+    void itShouldCorrectlyReadTheMessageAndTargetFile() throws IOException {
+        ConfigurationManager manager = new ConfigurationManager(CORRECT_CONFIG, MESSAGE_FILE, TARGET_FILE);
+
+        assertEquals(MESSAGE_COUNT, manager.getMessages().size());
+        assertEquals(TARGET_COUNT, manager.getPersons().size());
+    }
+
+    //todo tests si données malformatées
 }
