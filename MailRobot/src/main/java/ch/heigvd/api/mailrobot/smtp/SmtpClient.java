bb1dca2332fdc70f743aaf342aa1c086838dd5ca
BernardLhermite <stephane.marengo@hotmail.com>
2021-12-05 11:15:04 +0100

    Correction d'un bug dans SmtpClient: création d'un nouveau content à chaque fois Ajout d'un "Press any key to continue" dans build-image.sh

--- MailRobot/src/main/java/ch/heigvd/api/mailrobot/smtp/SmtpClient.java
+++ MailRobot/src/main/java/ch/heigvd/api/mailrobot/smtp/SmtpClient.java
@@ -52,7 +52,6 @@ public class SmtpClient {
 
         this.HOST = host;
         this.PORT = port;
-        content = new StringBuilder("Content-Type: text/plain; charset=utf-8");
     }
 
     /**
@@ -76,6 +75,7 @@ public class SmtpClient {
             socket = new Socket(HOST, PORT);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+            content = new StringBuilder("Content-Type: text/plain; charset=utf-8");
 
             if (!read(CODE_READY)
                     || !sendHello(domain)
@@ -90,7 +90,7 @@ public class SmtpClient {
             addDate();
             addFrom(from);
             addSubject(mail.getSubject());
-            addTo(mail.getRecipients());
+            addTo(recipients);
             addBody(mail.getBody());
 
             if (!sendContent()
